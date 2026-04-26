78608d0a02d7c16f239f72d7ce2ba1391a42e0b6
JorgeTI <jorgetorres240893@gmail.com>
2026-04-26 12:30:45 -0600

    calculadora con menu

--- calcu.py
+++ calcu.py
@@ -1,89 +1,38 @@
-def sumar():
+
+def operacion(opcion):
     while True:
         try:
-           suma = int(input("Ingresa un número: "))
-        except:
-           print("Valor no válido")
-           continue
-        try:
-            segundo_numero = int(input("+: "))
+            numero_1 = int(input("Inserta un número: "))
+            numero_2 = int(input("Segundo número: "))
         except:
             print("Valor no válido")
             continue
-        if suma > 0 or segundo_numero > 0:
-            resultado = suma + segundo_numero
-            print(resultado)
-            break
         
-
-def restar():
-    while True:
-        try:
-           resta = int(input("Ingresa un número: "))
-        except:
-           print("Valor no válido")
-           continue
-        try:
-            segundo_numero = int(input("-: "))
-        except:
-            print("Valor no válido")
-            continue
-        if resta > 0 or segundo_numero > 0:
-            resultado = resta - segundo_numero
-            print(resultado)
-            break
-
-
-
-def multiplicacion():
-    while True:
-        try:
-           multiplicar = int(input("Ingresa un número: "))
-        except:
-           print("Valor no válido")
-           continue
-        try:
-            segundo_numero = int(input("x: "))
-        except:
-            print("Valor no válido")
-            continue
-        if multiplicar > 0 or segundo_numero > 0:
-            resultado = multiplicar * segundo_numero
-            print(resultado)
-            break
-
-def division():
-    while True:
-        try:
-           dividir = int(input("Ingresa un número: "))
-        except:
-           print("Valor no válido")
-           continue
-        try:
-            segundo_numero = int(input("/: "))
-        except:
-            print("Valor no válido")
-            continue
-        if dividir > 0 or segundo_numero > 0:
-            resultado = dividir / segundo_numero
-            print(resultado)
-            break
+        if opcion == 1:
+            resultado = numero_1 + numero_2
+        elif opcion == 2:
+            resultado = numero_1 - numero_2
+        elif opcion == 3:
+            resultado = numero_1 * numero_2
+        elif opcion == 4:
+            resultado = numero_1 / numero_2
+        
+        print(resultado)
+        break
 
 print("1. Sumar")
 print("2. Restar")
 print("3. Multiplicar")
 print("4. Dividir")
 
-opcion = input("Elige una opción: ")
+opcion = int(input("Elige una opción: "))
+operacion(opcion)
 
-if opcion == "1":
-    sumar()
-elif opcion == "2":
-    restar()
-elif opcion == "3":
-    multiplicacion()
-elif opcion == "4":
-    division()
+
+        
+
+
+        
 
 
 
