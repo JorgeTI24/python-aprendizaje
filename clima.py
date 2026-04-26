0027a6e627935b3a8f9ccdd2609f1e90d04c446f
JorgeTI <jorgetorres240893@gmail.com>
2026-04-25 22:45:20 -0600

    script clima CDMX guarda en txt

--- clima.py
+++ clima.py
@@ -5,7 +5,6 @@ respuesta = requests.get("https://wttr.in/CDMX?format=j1")
 data = respuesta.json()
 
 clima = data['weather'][0]
-print(json.dumps(data["weather"][0], indent=4))
 
 
 
@@ -16,4 +15,17 @@ print("Temperatura promedio: ", clima["avgtempC"], "C")
 print("Máxima:", clima['maxtempC'], "°C")
 print("Mínima:", clima['mintempC'], "°C")
 print("Indice UV: ", clima["uvIndex"])
+
+with open("clima.txt", "w") as f:
+    f.write("=== Clima en CDMX ===\n")
+    f.write("Fecha: " + clima["date"] + "\n")
+    f.write("Temperatura promedio: " + clima["avgtempC"] + "C" + "\n")
+    f.write("Máxima:" + clima['maxtempC'] + "°C" + "\n")
+    f.write("Mínima:" + clima['mintempC'] + "°C" + "\n")
+    f.write("Indice UV: " + clima["uvIndex"] + "\n")
+
+
+
+
+
 
